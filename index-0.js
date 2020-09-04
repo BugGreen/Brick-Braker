bddec2db6e0d0d5dc0d1bfa0675db0d99ff960bf
BugGreen <nirojasbe@unal.edu.co>
2020-09-04 08:55:41 -0500

    04. Pintando en Canvas

--- index-0.js
+++ index-0.js
@@ -1,6 +1,15 @@
 const canvas = document.getElementById( 'lienzo' );
 
+//Esto permite 'colorear' el fondo:
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 canvas.style.backgroundColor = '#eeff00';
 
+//Esto es para conseguir un contexto gráfico:
+const ctx = canvas.getContext('2d');
+ctx.beginPath();
+ctx.lineWidth = '15';
+ctx.strokeStyle = 'red';
+ctx.moveTo( 0, 0 );
+ctx.lineTo( canvas.width, canvas.height );
+ctx.stroke();
