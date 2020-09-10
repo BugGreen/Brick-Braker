e5379a407def3a8fb84d1af9146518ec2d9b2053
BugGreen <nirojasbe@unal.edu.co>
2020-09-10 13:20:03 -0500

    taller #2

--- index-0.js
+++ index-0.js
@@ -7,9 +7,12 @@ canvas.style.backgroundColor = '#eeff00';
 
 //Esto es para conseguir un contexto gráfico:
 const ctx = canvas.getContext('2d');
-ctx.beginPath();
-ctx.lineWidth = '15';
-ctx.strokeStyle = 'red';
-ctx.moveTo( 0, 0 );
-ctx.lineTo( canvas.width, canvas.height );
-ctx.stroke();
+ctx.strokeRect(20, 15, 100, 25 );
+ctx.fillRect(20, 50, 100, 25);
+ctx.fillStyle = 'orangered';
+ctx.fillRect(20, 85, 100, 25);
+ctx.fillStyle = '#ff4500';
+ctx.fillRect(125, 85, 100, 25);
+ctx.fillStyle = '#ff450050';
+ctx.fillRect(230, 85, 100, 25);
+
