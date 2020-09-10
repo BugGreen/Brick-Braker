73a25f8b9759873a7920c0067d9afb3fc8218e51
BugGreen <nirojasbe@unal.edu.co>
2020-09-10 17:21:44 -0500

    05. Bricks

--- index-0.js
+++ index-0.js
@@ -7,9 +7,37 @@ canvas.style.backgroundColor = '#eeff00';
 
 //Esto es para conseguir un contexto gráfico:
 const ctx = canvas.getContext('2d');
-ctx.beginPath();
-ctx.lineWidth = '15';
-ctx.strokeStyle = 'red';
-ctx.moveTo( 0, 0 );
-ctx.lineTo( canvas.width, canvas.height );
-ctx.stroke();
+ctx.fillStyle = 'orangered';
+
+const bricksPerRow = 8;
+const numRows = 3;
+
+const gap = 5;
+
+// Tamaño de ventana - #separaciones / #Bloques
+const brickWidth = (window.innerWidth - (bricksPerRow - 1) * gap) / bricksPerRow;
+const brickHeight = brickWidth / 4;
+
+function drawBrick( column, row ) {
+    //Calcular la posición 'xPos' y 'yPos'
+    //Con base en en column y row
+    xPos = column * (brickWidth + gap);
+    yPos = row * (brickHeight + gap);
+
+    ctx.fillRect( xPos, yPos, brickWidth, brickHeight );
+}
+
+function drawRow( row ) {
+    for ( let x = 0; x < bricksPerRow; x++ ) {
+        drawBrick(x, row);
+    }
+}
+
+function drawWall() {
+    for (let y = 0; y < numRows; y++) {
+        drawRow( y );
+    }
+}
+
+drawWall();
+
