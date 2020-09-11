5a246e1b9a72fa916122039e82619f1c8362ade6
BugGreen <nirojasbe@unal.edu.co>
2020-09-11 13:39:44 -0500

    06. Arrays

--- index-0.js
+++ index-0.js
@@ -7,7 +7,6 @@ canvas.style.backgroundColor = '#eeff00';
 
 //Esto es para conseguir un contexto gráfico:
 const ctx = canvas.getContext('2d');
-ctx.fillStyle = 'orangered';
 
 const bricksPerRow = 8;
 const numRows = 3;
@@ -15,10 +14,12 @@ const numRows = 3;
 const gap = 5;
 
 // Tamaño de ventana - #separaciones / #Bloques
-const brickWidth = (window.innerWidth - (bricksPerRow - 1) * gap) / bricksPerRow;
-const brickHeight = brickWidth / 4;
+let brickWidth = (window.innerWidth - (bricksPerRow - 1) * gap) / bricksPerRow;
+let brickHeight = brickWidth / 4;
 
 function drawBrick( column, row ) {
+    ctx.fillStyle = 'orangered';
+
     //Calcular la posición 'xPos' y 'yPos'
     //Con base en en column y row
     xPos = column * (brickWidth + gap);
@@ -39,5 +40,21 @@ function drawWall() {
     }
 }
 
-drawWall();
+function recomputeBrickSize() {
+    //Primero redimensiona el Canvas:
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    //Acá se reajustan el tamaño de los ladrillos, que ahora son varibales:
+    brickWidth = (window.innerWidth - (bricksPerRow - 1) * gap) / bricksPerRow;
+    brickHeight = brickWidth / 4;
+}
 
+function drawGame() {
+    recomputeBrickSize();
+    drawWall();
+}
+
+//Apenas el usuario se registra:
+drawGame();
+//Cada vez que ocurre el suceso de redimensionamiento de ventana:
+window.onresize = drawGame;
