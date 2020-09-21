795f62cb32c0a869217721797e25e727e2958214
BugGreen <nirojasbe@unal.edu.co>
2020-09-21 18:48:15 -0500

    Objects - 0.5 (1/2) Index-06.js

--- index-0.js
+++ index-0.js
@@ -10,51 +10,86 @@ const ctx = canvas.getContext('2d');
 
 const bricksPerRow = 8;
 const numRows = 3;
-
 const gap = 5;
 
-// Tamaño de ventana - #separaciones / #Bloques
-let brickWidth = (window.innerWidth - (bricksPerRow - 1) * gap) / bricksPerRow;
-let brickHeight = brickWidth / 4;
+const bricks = [];
+
+function createWall() {
+    for ( let row = 0; row < numRows; row ++) {
+        for (let col = 0; col < bricksPerRow; col ++) {
+            // Creaciçon del objeto brick para cada col:
+            const brick = {
+                // Atributos:
+                c: col,
+                r: row,
+                w: 0,
+                h: 0,
+                color: 'orangered',
+                //Métodos:
+                paint: function () {
+                    // this. nos permite referenciar las propiedades
+                    // de este objeto.
+                    const x = this.c * (this.w + gap);
+                    const y = this.r * (this.h + gap);
+
+                    ctx.fillStyle = this.color;
+                    ctx.fillRect( x, y, this.w, this.h );
+                },
+
+                resize: function() {
+                    this.w = (window.innerWidth - (bricksPerRow - 1) * gap) / bricksPerRow;
+                    this.h = this.w / 4;
+                }
+
+            };
+
+            bricks.push( brick );
+        }
+    }
+}
 
 function drawBrick( column, row ) {
-    ctx.fillStyle = 'orangered';
+    const idx = row * bricksPerRow + column;
 
-    //Calcular la posición 'xPos' y 'yPos'
-    //Con base en en column y row
-    xPos = column * (brickWidth + gap);
-    yPos = row * (brickHeight + gap);
-
-    ctx.fillRect( xPos, yPos, brickWidth, brickHeight );
+    bricks[ idx ].paint();
 }
 
 function drawRow( row ) {
-    for ( let x = 0; x < bricksPerRow; x++ ) {
-        drawBrick(x, row);
+    for ( let column = 0; column < bricksPerRow; column++ ) {
+        drawBrick( column, row );
     }
 }
 
 function drawWall() {
-    for (let y = 0; y < numRows; y++) {
-        drawRow( y );
+    for (let row = 0; row < numRows; row++) {
+        drawRow( row );
     }
 }
 
-function recomputeBrickSize() {
+function resizeBricks() {
     //Primero redimensiona el Canvas:
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     //Acá se reajustan el tamaño de los ladrillos, que ahora son varibales:
-    brickWidth = (window.innerWidth - (bricksPerRow - 1) * gap) / bricksPerRow;
-    brickHeight = brickWidth / 4;
+    for ( let row = 0; row < numRows; row++ ) {
+        for (let col = 0; col < bricksPerRow; col ++) {
+            const idx = row * bricksPerRow + col;
+            bricks[ idx ].resize();
+        }
+    }
 }
 
 function drawGame() {
-    recomputeBrickSize();
+    resizeBricks();
     drawWall();
 }
 
-//Apenas el usuario se registra:
-drawGame();
 //Cada vez que ocurre el suceso de redimensionamiento de ventana:
 window.onresize = drawGame;
+
+// Crear la pared:
+createWall();
+
+// Pinta el juego apenas es cargada la página:
+drawGame();
+
